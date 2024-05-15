3d1140f0790eb9aeccba8e9f1c14f495e428db54
李斌 <libin@zulong.com>
2024-05-15 16:56:00 +0800

    修改svn命令：改为异步

--- lua/sal-proj-lua/svn_commands.lua
+++ lua/sal-proj-lua/svn_commands.lua
@@ -4,48 +4,48 @@ local svn_cmd = "TortoiseProc.exe"
 vim.api.nvim_create_user_command('SVNLog', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand("%")
 
-	local command_line = string.format('!%s  /command:log /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:log /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Update
 vim.api.nvim_create_user_command('SVNUpdate', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand("%")
 
-	local command_line = string.format('!%s  /command:update /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:update /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Commit
 vim.api.nvim_create_user_command('SVNCommit', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand(".")
 
-	local command_line = string.format('!%s  /command:commit /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:commit /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Revert
 vim.api.nvim_create_user_command('SVNRevert', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand(".")
 
-	local command_line = string.format('!%s  /command:revert /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:revert /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Cleanup
 vim.api.nvim_create_user_command('SVNCleanup', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand(".")
 
-	local command_line = string.format('!%s  /command:cleanup /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:cleanup /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Diff
 vim.api.nvim_create_user_command('SVNDiff', function(arg_table)
 	local path = arg_table.fargs[1] and vim.fn.expand(arg_table.fargs[1]) or vim.fn.expand("%")
 
-	local command_line = string.format('!%s  /command:diff /path:"%s"', svn_cmd, path)
-	vim.cmd(command_line)
+	local command_line = string.format('start %s  /command:diff /path:"%s"', svn_cmd, path)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
 
 -- Blame
@@ -54,7 +54,7 @@ vim.api.nvim_create_user_command('SVNBlame', function(arg_table)
 	local line = vim.fn.getcurpos()[2]
 
 	local command_line
-	command_line = string.format('!%s  /command:blame /path:"%s" /line:%d', svn_cmd, path, line)
+	command_line = string.format('start %s  /command:blame /path:"%s" /line:%d', svn_cmd, path, line)
 
-	vim.cmd(command_line)
+	io.popen(command_line)
 end, {nargs='*', complete="file"})
