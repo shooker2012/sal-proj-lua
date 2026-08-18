d267739bb82ccaac3b7cb12a39cad0487cf4c473
lovelywzz@gmail.com <lovelywzz@gmail.com>
2026-08-18 17:14:58 +0800

    调整vimproj加载顺序

--- lua/sal-proj-lua/init.lua
+++ lua/sal-proj-lua/init.lua
@@ -11,11 +11,10 @@ local custom_project_type = {"lua"}
 
 local project_pattern_table = { "*.vimproj", "_vimproj" }
 
-function InitSalProj()
-	vim.o.ft = "lua"
-
+function InitSalProj(args)
+	local bufnr = args and args.buf or vim.api.nvim_get_current_buf()
 	local file_type = nil
-	local file_name = vim.fn.expand("%")
+	local file_name = vim.api.nvim_buf_get_name(bufnr)
 	local s = file_name:find("%.")
 	if s then
 		file_type = file_name:sub(1, s-1):lower()
@@ -24,21 +23,23 @@ function InitSalProj()
 	-- Base config.
 	vim.opt.autochdir = false
 
-	local current_win = vim.api.nvim_get_current_win()
-
-	vim.cmd("Neotree action=show reveal_force_cwd=true")
-	vim.cmd("Copen")
-
-	vim.api.nvim_set_current_win(current_win)
-
 	for i, v in ipairs(custom_project_type) do
 		if file_type == v:lower() then
 			require("sal-proj-lua/configs/"..v)
 		end
 	end
 
-	-- custom config in vimproj file
+	-- Load project settings before the Lua FileType event starts language
+	-- tooling that depends on those settings.
 	dofile(file_name)
+	vim.bo[bufnr].filetype = "lua"
+
+	local current_win = vim.api.nvim_get_current_win()
+
+	vim.cmd("Neotree action=show reveal_force_cwd=true")
+	vim.cmd("Copen")
+
+	vim.api.nvim_set_current_win(current_win)
 end
 
 function SetupAutoCommand()
